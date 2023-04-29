fca4e4dabd354d140800292a8069c62c7459afbf
princewilling <princewillingoo@gmail.com>
2023-04-29 22:05:43 +0100

    create database models & register admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Room, Topic, Messaage
 
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Messaage)
+admin.site.register(Topic)
