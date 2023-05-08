9d1aaef4612d93c623d787c38f3dc8d586593895
princewilling <princewillingoo@gmail.com>
2023-05-08 16:06:29 +0100

    set-up static files

--- studyhub/settings.py
+++ studyhub/settings.py
@@ -130,6 +130,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = # for user uploaded medias
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
